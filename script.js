e51512b4d791866ff52b12806dfd0d7b6fa75af1
Diyana Konsulova <96625132+diyanaKK@users.noreply.github.com>
2022-03-24 18:53:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var character = document.getElementById("character")
+var block = document.getElementById("block")
+function jump() {
+    if (character.classList != "animate") {
+        character.classList.add("animate")
+    }
+    setTimeout(function () {
+        character.classList.remove("animate")
+    }, 500)
+}
+// function checkDead(){
+//     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+//     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+//     if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+//         //block.style.animation = "none"
+//         //block.style.display = "none"
+//         alert("Game over");
+//     }
+// }
+
+setInterval(checkDead, 10);
